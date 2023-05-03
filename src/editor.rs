85246a11230478acb0075da954cee03f68af8f8e
Takuya Azumaguchi <catchballer@gmail.com>
2023-05-03 14:51:29 +0900

    Fix issue of flicking cursor

--- src/editor.rs
+++ src/editor.rs
@@ -25,6 +25,7 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), io::Error> {
+        Terminal::cursor_hide();
         Terminal::clear_screen();
         Terminal::cursor_position(0, 0);
         if self.should_quit {
@@ -33,6 +34,7 @@ impl Editor {
             self.draw_rows();
             Terminal::cursor_position(0, 0);
         }
+        Terminal::cursor_show();
         Terminal::flush()
     }
 
@@ -46,7 +48,7 @@ impl Editor {
     }
 
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height {
+        for _ in 0..self.terminal.size().height - 1 {
             println!("~\r");
         }
     }
