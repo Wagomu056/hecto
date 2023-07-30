2cf9f6e30bcf1176776b9d720dabc35f4fd09928
Takuya Azumaguchi <catchballer@gmail.com>
2023-07-30 19:37:36 +0900

    Added cursor movement function

--- src/terminal.rs
+++ src/terminal.rs
@@ -2,6 +2,7 @@ use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
+use crate::Position;
 
 pub struct Size {
     pub width: u16,
@@ -32,9 +33,12 @@ impl Terminal {
     pub fn clear_current_line() {
         print!("{}", termion::clear::CurrentLine);
     }
-    pub fn cursor_position(x: u16, y: u16) {
+    pub fn cursor_position(position: &Position) {
+        let Position{x, y} = position;
         let x = x.saturating_add(1);
         let y = y.saturating_add(1);
+        let x = x as u16;
+        let y = y as u16;
         print!("{}", termion::cursor::Goto(x, y));
     }
     pub fn flush() -> Result<(), io::Error>{
