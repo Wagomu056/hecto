5243d391644c6058a60670810957254ad2f54a6b
Takuya Azumaguchi <catchballer@gmail.com>
2023-08-11 18:24:49 +0900

    Change to scrollable

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@
 
 mod editor;
 mod terminal;
+mod document;
+mod row;
 
 use editor::Editor;
 pub use terminal::Terminal;
 pub use editor::Position;
+pub use row::Row;
+pub use document::Document;
 
 fn main() {
     Editor::default().run();
